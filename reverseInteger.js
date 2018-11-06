eb56b6c7eae99a394e566d90086ce70a24927404
Alvin Galit <aroblesgalit@gmail.com>
2018-11-05 19:25:41 -0800

    no change

--- reverseInteger.js
+++ reverseInteger.js
@@ -2,11 +2,13 @@
  * @param {number} x
  * @return {number}
  */
+
+var newNum = "";
+
 var reverse = function(x) {
-    var newNum = "";
     for (var i = x.length - 1; i >= 0; i--) {
         return newNum.push[i];
     }
 };
 
-console.log(function(321));
+console.log(reverse(123));
