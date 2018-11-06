40a824437ddd0a7526fda27d034b4e9471b82ff8
Alvin Galit <aroblesgalit@gmail.com>
2018-11-05 18:53:23 -0800

    new leetcode challenge

---
+++ reverseInteger.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    var newNum = "";
+    for (var i = x.length - 1; i >= 0; i--) {
+        return newNum.push[i];
+    }
+};
+
+console.log(function(321));
